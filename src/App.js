df19beb87af3181ebaa89bf6c69a30f7e71d8946
wilham reyssan <wilhamr.reyssan@gmail.com>
2019-09-30 02:50:25 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './bkg.png';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p
+        id="myP"> Seminar Calendar
+        <script>
+          document.getElementById("myP").style.fontFamily = "Courier New";
+        </script>
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
