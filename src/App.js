958729187fe90ebfce098a17d12c701e377121fd
wilham reyssan <wilhamr.reyssan@gmail.com>
2019-09-30 05:05:06 -0400

    set layout

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="myP">Seminar Calendar</p>        
+        <div class="content">
+          <table border="1px">
+            <tr>
+              <td class="logo-area"><img src={logo} className="App-logo" alt="logo" /></td>
+              <td class="wrapper-area">
+                <h1 class="title">Seminar Calendar</h1>
+                <div class="form">
+                  <form action="">
+                    <input placeholder="form nama"/>
+                    <input placeholder="form password"/>
+                    <button class="button-login">login</button>  
+                  </form>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
       </header>
     </div>
   );
