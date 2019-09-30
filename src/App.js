fc8c8975c9228cff331f4e068bf6e68150350bb8
wilham reyssan <wilhamr.reyssan@gmail.com>
2019-09-30 02:57:03 -0400

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p
-        id="myP"> Seminar Calendar
+        id="myP"> Kalendar Seminar
         <script>
           document.getElementById("myP").style.fontFamily = "Courier New";
         </script>
