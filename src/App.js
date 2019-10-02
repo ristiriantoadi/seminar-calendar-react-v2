c3bd5231312a394dc35283b452f652a00d941b8b
Ristirianto Adi <ristiriantoadi@gmail.com>
2019-10-02 18:10:32 +0800

    desain login page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import React from 'react';
-import logo from './bkg.png';
-import './App.css';
-
+import React from "react";
+import logo from "./bkg.png";
+import "./App.css";
+// import Button from "@material-ui/core/Button";
+import Login from "./Components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div class="content">
-          <table border="1px">
-            <tr>
-              <td class="logo-area"><img src={logo} className="App-logo" alt="logo" /></td>
-              <td class="wrapper-area">
-                <h1 class="title">Kalendar Seminar</h1>
-                <div class="form">
-                  <form action="">
-                    <input placeholder="form nama"/>
-                    <input placeholder="form password"/>
-                    <button class="button-login">login</button>  
-                  </form>
-                </div>
-              </td>
-            </tr>
-          </table>
-        </div>
-      </header>
-    </div>
-  );
+  return <Login></Login>;
 }
-
-
 
 export default App;
